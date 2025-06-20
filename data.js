a27eb4fa4a45e7fec8d5200f7af69b4f5f461f40
Asimsanu <asimsanu@studioemfiz.com>
2025-06-20 06:16:19 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama3",
+      "name": "Panorama(3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.367861703881891,
+        "pitch": -0.030076109228275527,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5113988007812917,
+          "pitch": 0.3960715369512009,
+          "rotation": 0,
+          "target": "2-panorama5"
+        },
+        {
+          "yaw": 0.15900521579656512,
+          "pitch": 0.17365148222725324,
+          "rotation": 0,
+          "target": "1-panorama4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama4",
+      "name": "Panorama(4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5335341782074252,
+          "pitch": 0.3125300064048169,
+          "rotation": 0,
+          "target": "2-panorama5"
+        },
+        {
+          "yaw": -0.9046995166468985,
+          "pitch": 0.13366805179781593,
+          "rotation": 0,
+          "target": "0-panorama3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-panorama5",
+      "name": "Panorama(5)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.08608265476386556,
+        "pitch": 0.19204987512106086,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.039949158360636616,
+          "pitch": 0.23729310003100146,
+          "rotation": 0,
+          "target": "3-panorama6"
+        },
+        {
+          "yaw": -2.0838677347129995,
+          "pitch": 0.09596365849921185,
+          "rotation": 0,
+          "target": "0-panorama3"
+        },
+        {
+          "yaw": 2.2557146699210238,
+          "pitch": 0.50266815833373,
+          "rotation": 0,
+          "target": "1-panorama4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-panorama6",
+      "name": "Panorama(6)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.109151628870894,
+          "pitch": 0.6832637262715338,
+          "rotation": 0,
+          "target": "2-panorama5"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "b2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
